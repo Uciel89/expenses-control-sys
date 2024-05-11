4bb1b76b1cafe2083f13b63873e6b5b4aaff04f8
LeonardoLaRosa <116355251+Leonardo1190@users.noreply.github.com>
2024-05-10 21:25:05 -0300

    Creacion de entidades v3

--- api/src/main/java/com/expenses/control/sys/api/persistence/entities/Cuenta.java
+++ api/src/main/java/com/expenses/control/sys/api/persistence/entities/Cuenta.java
@@ -1,18 +1,25 @@
-package persistence.entities;
+package com.expenses.control.sys.api.persistence.entities;
 
 import io.micrometer.core.annotation.Counted;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 @Entity
+@Table(name = "cuentas")
 public class Cuenta {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "id_cuenta",nullable = false)
     private Long idCuenta;
-    @Column
+    @Basic
     private String nombre;
-    @Column
     private double saldo;
+    @OneToMany(mappedBy = "idIngreso")
+    private List<Ingreso> ingresoList;
+    @ManyToOne
+    @JoinColumn(name = "id_gasto")
+    private Gasto gasto;
 }
---
+++ api/src/main/java/com/expenses/control/sys/api/persistence/entities/Gasto.java
@@ -0,0 +1,26 @@
+package com.expenses.control.sys.api.persistence.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.context.annotation.EnableMBeanExport;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "gastos")
+public class Gasto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id_gasto",nullable = false)
+    private Long idGasto;
+    @Basic
+    private double monto;
+    private LocalDateTime fecha;
+    @ManyToOne
+    @JoinColumn(name = "id_categoria")
+    private Categoria categoria;
+    @OneToMany(mappedBy = "idCuenta")
+    private List<Cuenta> cuentaList;
+}
