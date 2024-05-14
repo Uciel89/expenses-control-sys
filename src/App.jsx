9a0cdb7defd4885ec7698592a50810fd410758a4
Uciel Daró <73118144+Uciel89@users.noreply.github.com>
2024-05-13 21:29:05 -0300

    Home - Creación de header / contenido principal / footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { Route } from 'react-router-dom'
 import './App.css'
+
+import { BrowserRouter, Routes, Router } from 'react-router-dom'
+import HomePage from './pages/HomePage/HomePage'
 
 function App() {
 
   return (
-    <></>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/*' element={<HomePage />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
