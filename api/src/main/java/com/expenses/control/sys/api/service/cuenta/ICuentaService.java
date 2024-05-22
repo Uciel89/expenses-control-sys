e5a0446b5bc33ec78b75b49efb36c5a5a2959674
Leonardo La Rosa <leonardolarosa1190@gmail.com>
2024-05-21 21:09:41 -0300

    Inicio y configuracion de Open Api con Swagger

--- api/src/main/java/com/expenses/control/sys/api/service/cuenta/ICuentaService.java
+++ api/src/main/java/com/expenses/control/sys/api/service/cuenta/ICuentaService.java
@@ -1,10 +1,12 @@
 package com.expenses.control.sys.api.service.cuenta;
 
+import com.expenses.control.sys.api.dto.cuenta.CuentaDto;
 import com.expenses.control.sys.api.model.entities.Cuenta;
 
 import java.util.List;
 
 public interface ICuentaService {
+    
     List<Cuenta> getAllCuentas();
     Cuenta getCuentaById(long id);
 
