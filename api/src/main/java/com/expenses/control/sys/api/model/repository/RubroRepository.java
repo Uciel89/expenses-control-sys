a5861ecd94e2b4f0a4f6b9ebbc45c948e01d4144
Uciel Daró <73118144+Uciel89@users.noreply.github.com>
2024-06-27 11:11:45 -0300

    Delete api directory

--- api/src/main/java/com/expenses/control/sys/api/model/repository/RubroRepository.java
+++
@@ -1,8 +0,0 @@
-package com.expenses.control.sys.api.model.repository;
-
-import com.expenses.control.sys.api.model.entities.Rubro;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RubroRepository extends JpaRepository<Rubro,Long> {
-    Rubro findByNombreRubro(String nombreRubro);
-}
